0637028a8de340128b432c319b324cc928ab4f77
ilmannafian04 <milmannafian04@gmail.com>
2021-10-04 14:04:00 +0700

    refactor: moved index style from layout to index route

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,1 @@
-<main class="main-container">
-  <slot />
-</main>
-
-<style>
-  .main-container {
-    background-color: #222;
-    position: relative;
-    width: 100%;
-    height: 100%;
-    color: #333;
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue',
-      sans-serif;
-  }
-</style>
+<slot />
