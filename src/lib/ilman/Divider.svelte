249524eeea706f9d61f92df32a4d3f74b590f5b9
ilmannafian04 <milmannafian04@gmail.com>
2021-10-09 22:19:22 +0700

    feat: initial design

---
+++ src/lib/ilman/Divider.svelte
@@ -0,0 +1,20 @@
+<div class="container">
+  {#each Array(3) as _, i (i)}
+    <span class="dot" />
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 30px;
+  }
+
+  .dot {
+    background-color: #33cb56;
+    border-radius: 50%;
+    height: 15px;
+    width: 15px;
+  }
+</style>
---
+++ src/routes/ilman.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // just for future proofing when we decided to use ssr and serve static content from elsewhere
+  import { assets } from '$app/paths';
+  import Divider from '$lib/ilman/Divider.svelte';
+</script>
+
+<div class="container">
+  <img src="{assets}/ilman/stare.jpg" alt="me" height="200px" width="200px" />
+  <Divider />
+  <div>
+    <p>asdasd</p>
+    <p>asdasd</p>
+  </div>
+</div>
+
+<style>
+  .container {
+    background-color: #0d1117;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: whitesmoke;
+    column-gap: 20px;
+  }
+</style>
