29d493d49b0c10dc65262ff81f1efb8a7a67e064
wahyuadt <wahyuadt9@gmail.com>
2021-10-11 02:32:27 +1000

    feat: initiate tailwindcss

--- src/routes/wahyuadt.svelte
+++ src/routes/wahyuadt.svelte
@@ -1,7 +1,8 @@
 <script>
   import { assets } from '$app/paths';
+  import '../app.css';
 </script>
 
-<div>halo</div>
+<div class="m-4">halo</div>
 <div class="container">hahah</div>
 <img src="{assets}/wahyu/violet.jpg" alt="violet" />
