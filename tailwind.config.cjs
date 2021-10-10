1ed27049244b4ae3c7df1af3593671925f516643
wahyuadt <wahyuadt9@gmail.com>
2021-10-11 02:39:44 +1000

    refactor: lint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 const config = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {},
   },
 
-  plugins: []
+  plugins: [],
 };
 
 module.exports = config;
