92dce0b7bc4801a34eaf01a44d2b20e491eccafb
Naufal Irbahhana <irbahananaufal@gmail.com>
2021-10-11 01:01:30 +0700

    initial commit

---
+++ src/routes/irbahhana.svelte
@@ -0,0 +1,53 @@
+<script>
+
+</script>
+
+
+<div class='main-container'>
+
+  <div class='header'>
+    <p class='home-link'><a href='/'> Back to main page</a></p>
+    <h1 class='title'>
+      Werk in poggiez
+    </h1>
+    <img
+      src='https://64.media.tumblr.com/1fdc557ee8ae894407fa1c9a862729ea/95a9c4952480baec-2a/s500x750/5cece191cc55299e515142e5d4a46170c8f461de.gifv' />
+  </div>
+</div>
+
+<style>
+    p {
+        margin-block-start: 0;
+        margin-block-end: 0;
+    }
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+    }
+
+    .main-container {
+        width: 100%;
+        height: 100vh;
+        background-color: #474c5b;
+    }
+
+    .text {
+        align-items: start;
+    }
+
+    a {
+        color: white;
+    }
+
+    .title {
+        color: white;
+    }
+
+    h1 {
+    }
+</style>
