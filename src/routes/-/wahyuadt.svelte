d14b2fb7c1331aad3967e5cd75e535a5b1305670
Danny August Ramaputra <daystram@gmail.com>
2021-10-11 04:48:25 +1000

    refactor: added member subdir

--- src/routes/-/wahyuadt.svelte
+++ src/routes/-/wahyuadt.svelte
@@ -1,8 +1,9 @@
 <script>
   import { assets } from '$app/paths';
-  import '../app.css';
+  import '../../tailwind.css';
 </script>
 
 <div class="m-4">halo</div>
 <div class="container">hahah</div>
 <img src="{assets}/wahyu/violet.jpg" alt="violet" />
+4
