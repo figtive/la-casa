4d7aa21fb3668f8de29e91368c4e3ae1d7bef7d1
Danny August Ramaputra <daystram@gmail.com>
2021-10-11 19:03:54 +1000

    refactor: updated title lib

--- src/routes/-/wahyuadt.svelte
+++ src/routes/-/wahyuadt.svelte
@@ -1,6 +1,9 @@
 <script>
+  import Title from '$lib/Title.svelte';
   import { assets } from '$app/paths';
 </script>
+
+<Title title="wahyuadt" description="Wahyu Ananda Duli Tokan" />
 
 <div class="m-16">halo</div>
 <div class="container">hahah</div>
