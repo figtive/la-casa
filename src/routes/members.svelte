a197a2ac41d2a56816bef9bc4767e6a34f7ed6b9
Danny August Ramaputra <daystram@gmail.com>
2021-10-11 18:32:11 +1000

    feat: added member page

---
+++ src/routes/members.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Title from '$lib/Title.svelte';
+
+  const members = [
+    {
+      name: 'Danny August Ramaputra',
+      username: 'daystram',
+      path: 'daystram',
+    },
+    {
+      name: 'Wahyu Ananda Duli Tokan',
+      username: 'wahyuadt',
+      path: 'wahyuadt',
+    },
+  ];
+</script>
+
+<Title title="Members" />
+
+<div class="main">
+  <div class="container mx-auto">
+    <h1 class="title-h1">Members</h1>
+    <ul>
+      {#each members as member (member.path)}
+        <li class="mb-4">{member.name} <a href="/-/{member.path}" class="link">{member.username}</a></li>
+      {/each}
+    </ul>
+  </div>
+</div>
