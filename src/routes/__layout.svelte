b274f14a4c5e5cb7391d96fed8b6213e2c4fea37
Danny August Ramaputra <daystram@gmail.com>
2021-10-15 05:02:04 +1000

    fix: main bottom padding

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -34,6 +34,7 @@
 
   .main {
     padding-top: $navbar-height;
+    padding-bottom: 128px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue',
       sans-serif;
     color: $color-secondary;
