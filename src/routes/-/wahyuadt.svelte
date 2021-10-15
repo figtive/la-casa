87131f2e9aed329ffee020560a63e7f809802cc1
wahyuadt <wahyuadt9@gmail.com>
2021-10-15 19:53:18 +1000

    refactor: remove tailwindcss

--- src/routes/-/wahyuadt.svelte
+++ src/routes/-/wahyuadt.svelte
@@ -1,10 +1,9 @@
 <script>
   import Title from '$lib/Title.svelte';
-  import { assets } from '$app/paths';
 </script>
 
 <Title title="wahyuadt" description="Wahyu Ananda Duli Tokan" />
 
 <div class="m-16">halo</div>
 <div class="container">hahah</div>
-<img src="{assets}/wahyu/violet.jpg" alt="violet" />
+<!-- <img src="{assets}/wahyu/violet.jpg" alt="violet" /> -->
